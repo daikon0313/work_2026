a912b4a9b78149919be47ad30bb72ebbd51ebd26
Daisuke Harato <d.harato@datumstudio.jp>
2026-01-06 15:19:23 +0900

    修正

--- 3_homepage/src/data/articles.ts
+++ 3_homepage/src/data/articles.ts
@@ -3,14 +3,8 @@ import { parseMarkdown } from '../utils/parseMarkdown'
 
 // マークダウンファイルを動的インポート
 import article001 from './articles/databricks/001.md?raw'
-import article002 from './articles/002.md?raw'
-import article003 from './articles/003.md?raw'
-import article004 from './articles/004.md?raw'
 
 // マークダウンファイルをパースして記事配列を作成
 export const articles: Article[] = [
   parseMarkdown(article001),
-  // parseMarkdown(article002),
-  // parseMarkdown(article003),
-  // parseMarkdown(article004),
 ]
